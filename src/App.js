18bb548b09acabbbb344bdb90272452707391e97
Gabby Recny <gjtrecny@gmail.com>
2023-01-24 14:52:39 -0600

    Remove unecessary code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import Board from './Components/Board/Board';
-import Game from './Components/Game/Game';
 
-function App() {
-  return (
-    <>
-      <h1>Tic-Tac-Toe!</h1>
+
+const App = () => {
+
+return (
+  <>
+    <h1>Tic-Tac-Toe!</h1>
       <Board />
-      <Game />
-    </>
-  );
+  </>
+);
 }
 
 export default App;
