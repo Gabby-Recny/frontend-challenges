d2cee859e81a1b761cef0f5ed190e9c70e4c4a2a
Gabby Recny <gjtrecny@gmail.com>
2023-02-17 12:48:32 -0600

    chore: Rename component for better readability

--- src/Components/RestartGame/RestartGame.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RestartGame = () => {
-    return (
-        <h2>Restart game?</h2>
-    )
-}
---
+++ src/Components/RestartGameModal/RestartGameModal.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './RestartGameModal.css';
+import xIcon from '../../assets/icon-x.svg';
+import oIcon from '../../assets/icon-o.svg';
+
+
+const RestartGameModal = ({isModalOpen, closeModal, clearGameBoard}) => {
+    if (!isModalOpen) {
+        return null;
+    }
+
+    const restartGame = () => {
+        closeModal()
+        clearGameBoard()
+    }
+
+    return (
+        <section className="restart-game-modal-container">
+            <div className="restart-game-modal-icons">
+                    <img src={xIcon} alt="X token"/>
+                    <img src={oIcon} alt="O token"/>
+
+            </div>
+            <h3>Would you like to restart the game?</h3>
+            <div className="restart-game-modal-btn-container">
+                <button onClick={() => restartGame()}>Start New Game</button>
+                <button onClick={() => closeModal()}>Continue Playing</button>
+            </div>
+        </section>
+    )
+}
+
+export default RestartGameModal;
